1e0ddbd3b145d561f33f1257c054ff67aa8183ac
Rodríguez Garrucho, Oscar <orodriguezg@eservicios.indra.es>
2024-09-23 20:26:41 +0200

    Initial Commit

---
+++ RickAndMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyApp
+//
+//  Created by Enrique Poyato Ortiz on 23/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Enrique Poyato Ortiz on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
